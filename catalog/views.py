15d7e932b31abdde96eaed354415a76e93b2e40c
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-09-23 20:47:52 +0300

    Реализована обработка заполнения формы обратной связи

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -6,4 +7,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f"{name}, указанные Вами телефон и сообщение получены<br>Телефон: {phone}<br>Сообщение: {message}")
     return render(request, 'catalog/contacts.html')
