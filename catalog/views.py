de17047db186ebb2d488d44d043df1b946a5c18a
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-09-23 21:01:07 +0300

    Отформатировано линтерами

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    return render(request, "catalog/home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        return HttpResponse(f"{name}, указанные Вами телефон и сообщение получены<br>Телефон: {phone}<br>Сообщение: {message}")
-    return render(request, 'catalog/contacts.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(
+            f"{name}, указанные Вами телефон и сообщение получены<br>Телефон: {phone}<br>Сообщение: {message}"
+        )
+    return render(request, "catalog/contacts.html")
