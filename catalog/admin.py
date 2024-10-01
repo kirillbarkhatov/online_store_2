02bbc21a653ff069125e619dc25cb07379486c91
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-01 22:39:12 +0300

    Настроена админка

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Настройки отображения модели в админке"""
+
+    list_display = ("id", "name", ) # вывод колонок
+    search_fields = ("name",) # поиск по указанному полю/полям
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Настройки отображения модели в админке"""
+
+    list_display = ("id", "name", "price", "category",)  # вывод колонок
+    list_filter = ("category",) # добавление фильтра по указанному полю
+    search_fields = ("name", "description",)  # поиск по указанному полю/полям
