fed7189c8f8782cbfea1b0f5c0aaeef71200c497
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-02 00:46:43 +0300

    Вывод в консоль для котроллера главной страницы

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from catalog.models import Product
 
 
 # Create your views here.
 def home(request):
+
+    # вывод в консоль 5 последних созданных продуктов
+    products = Product.objects.all().order_by("-created_at")[:5]
+    for product in products:
+        print(product.name)
+
     return render(request, "catalog/home.html")
 
 
