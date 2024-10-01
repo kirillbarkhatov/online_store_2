01bc4b53c6c53475392c1780488c8c4e0261183c
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-01 22:21:36 +0300

    Описаны модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    """Модель для описания категорий"""
+
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name', ]
+
+
+class Product(models.Model):
+    """Модель для описания продуктов"""
+
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL(), null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена в рублях')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', ]
+
+
+
+
