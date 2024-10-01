1e014defd79a117273cad86ede6e9b445d2303bf
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-01 22:28:20 +0300

    Выполнена миграция

--- catalog/models.py
+++ catalog/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='images/', verbose_name='Изображение')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL(), null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена в рублях')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
