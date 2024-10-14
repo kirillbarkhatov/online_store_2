e417415e77964328dc71fe5595e089d2b95cdbb0
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-15 00:56:24 +0300

    Создана модель для блога

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.template.defaultfilters import title
+
 
 # Create your models here.
+class BlogEntry(models.Model):
+    """ Модель для блоговой записи"""
+
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(upload_to="blog", null=True, blank=True, verbose_name="Превью")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(verbose_name="Отметьте для публикации")
+    view_count = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+        ordering = [
+            "created_at",
+        ]
