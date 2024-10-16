abcc5a732748dd30d8efc27f190c691d3d141c07
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-16 19:41:20 +0300

    Отправка почты

--- config/settings.py
+++ config/settings.py
@@ -123,14 +123,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Настройки отправки почты
+# Настройки отправки почты для mail.ru
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'barchatovkirill@mail.ru'
-EMAIL_HOST_PASSWORD = 'uLfGpDPzrpa0be4dqxeP'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 # Static files (CSS, JavaScript, Images)
