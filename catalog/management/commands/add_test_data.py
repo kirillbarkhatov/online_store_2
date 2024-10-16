e6d28f0567b44e79eb6e8d2e0aaae05bb65092bf
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-10-16 20:04:25 +0300

    Обновление фикстур

--- catalog/management/commands/add_test_data.py
+++ catalog/management/commands/add_test_data.py
@@ -2,6 +2,7 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 from catalog.models import Category, Product
+from blog.models import BlogEntry
 
 
 class Command(BaseCommand):
@@ -11,9 +12,13 @@ class Command(BaseCommand):
         # Удаляем существующие записи
         Category.objects.all().delete()
         Product.objects.all().delete()
+        BlogEntry.objects.all().delete()
 
         # Добавляем данные из фикстур
         call_command("loaddata", "categories_fixture.json", format="json")
         self.stdout.write(self.style.SUCCESS("Категории загружены из фикстур успешно"))
         call_command("loaddata", "products_fixture.json", format="json")
         self.stdout.write(self.style.SUCCESS("Продукты загружены из фикстур успешно"))
+        call_command("loaddata", "blogentry_fixture.json", format="json")
+        self.stdout.write(self.style.SUCCESS("Записи блога загружены из фикстур успешно"))
+
